adfc8199fb77dd2ec222a3665a96e2b0469f147a
mridul.kumar <mridul.kumar@procol.in>
2022-02-27 20:53:47 +0530

    added responsive layout

---
+++ lib/responsive/mobile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MobileScreenLayout extends StatefulWidget {
+  const MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  _MobileScreenLayoutState createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Mobile"),
+    );
+  }
+}
---
+++ lib/responsive/web_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WebScreenLayout extends StatefulWidget {
+  const WebScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  _WebScreenLayoutState createState() => _WebScreenLayoutState();
+}
+
+class _WebScreenLayoutState extends State<WebScreenLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("web"),
+    );
+  }
+}
